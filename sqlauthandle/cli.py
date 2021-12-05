9a41b6b3b1ff2092e32edf3b87e4c80d55189f9c
mentoldo <matias.alfonso@gmail.com>
2021-12-04 23:07:45 -0300

    Se agregó CLI interface

--- sqlauthandle/cli.py
+++ sqlauthandle/cli.py
@@ -1,14 +1,21 @@
 """Console script for sqlauthandle."""
 import sys
 import click
-
+from sqlauthandle.set_credentials import set_credentials, gui_set_cred
 
 @click.command()
-def main(args=None):
+@click.option('--gui', default='F', help='GUI interface. T o F')
+def main(gui):
     """Console script for sqlauthandle."""
     click.echo("Replace this message by putting your code into "
                "sqlauthandle.cli.main")
     click.echo("See click documentation at https://click.palletsprojects.com/")
+    
+    if gui == 'F': 
+        set_credentials()
+    else:
+        gui_set_cred()
+        
     return 0
 
 
