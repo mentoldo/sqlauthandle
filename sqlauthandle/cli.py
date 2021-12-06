fd048a331e3756d40080ef2edd3e6335b0cc2769
mentoldo <matias.alfonso@gmail.com>
2021-12-06 19:04:51 -0300

    Se corrigió mensaje de interfaz CLI

--- sqlauthandle/cli.py
+++ sqlauthandle/cli.py
@@ -7,9 +7,8 @@ from sqlauthandle.set_credentials import set_credentials, gui_set_cred
 @click.option('--gui', default='F', help='GUI interface. T o F')
 def main(gui):
     """Console script for sqlauthandle."""
-    click.echo("Replace this message by putting your code into "
-               "sqlauthandle.cli.main")
-    click.echo("See click documentation at https://click.palletsprojects.com/")
+    click.echo("Sqlauthandle")
+    click.echo("See sqlauthandle documentation at https://sqlauthandle.readthedocs.io/")
     
     if gui == 'F': 
         set_credentials()
